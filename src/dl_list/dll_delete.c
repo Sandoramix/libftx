318431b7742b3ea851289e7ee5a06e3e736e4450
Oleksandr Dudniak <odudniak@student.42firenze.it>
2024-01-17 17:51:17 +0100

    [fix] bad handling when the list is of size 1

--- src/dl_list/dll_delete.c
+++ src/dl_list/dll_delete.c
@@ -6,7 +6,7 @@
 /*   By: odudniak <odudniak@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 12:23:56 by odudniak          #+#    #+#             */
-/*   Updated: 2024/01/14 15:42:18 by odudniak         ###   ########.fr       */
+/*   Updated: 2024/01/17 17:48:09 by odudniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,8 +42,13 @@ t_dllist	*dll_delhead(t_dllist **head)
 	if (!head || !*head)
 		return (NULL);
 	oldhead = *head;
-	*head = oldhead->next;
-	(*head)->prev = oldhead->prev;
+	if (dll_size(*head) == 1)
+		*head = NULL;
+	else
+	{
+		*head = oldhead->next;
+		(*head)->prev = oldhead->prev;
+	}
 	free(oldhead->val);
 	free(oldhead);
 	return (*head);
@@ -52,14 +57,21 @@ t_dllist	*dll_delhead(t_dllist **head)
 t_dllist	*dll_deltail(t_dllist **head)
 {
 	t_dllist	*oldtail;
+	t_dllist	*pretail;
 
 	if (!head || !*head)
 		return (NULL);
 	oldtail = dll_gettail(*head);
-	if (oldtail->prev && oldtail->prev != oldtail && oldtail->next != oldtail)
-		oldtail->prev->next = oldtail->next;
-	if (oldtail->next && oldtail->next != oldtail && oldtail->prev != oldtail)
-		oldtail->next->prev = oldtail->prev;
+	if (dll_size(*head) == 1)
+		*head = NULL;
+	else
+	{
+		pretail = oldtail->prev;
+		if (pretail && oldtail->next != pretail)
+			pretail->next = oldtail->next;
+		if (oldtail->next)
+			oldtail->next->prev = pretail;
+	}
 	free(oldtail->val);
 	free(oldtail);
 	return (*head);
